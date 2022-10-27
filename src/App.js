3cae7a1edaccede13aa80264b265a0d84aa5facd
Lawrence O. Caulker <locaulker@gmail.com>
2022-10-27 15:26:11 -0400

    completed error handling

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import "./App.css"
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
     { text: "Do all exercises!", id: "g1" },
-    { text: "Finish the course!", id: "g2" }
+    { text: "Finish the course!", id: "g2" },
   ])
 
   const addGoalHandler = enteredText => {
     setCourseGoals(prevGoals => {
       const updatedGoals = [...prevGoals]
-      updatedGoals.unshift({ text: enteredText, id: "goal1" })
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() })
       return updatedGoals
     })
   }
